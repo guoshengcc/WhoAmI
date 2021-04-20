fbbd06d9c6a9ab44f745e0bc767b31f8ce368468
guoshenga <guoshenga@digitalchina.com>
2021-04-20 15:59:37 +0800

    add show service ip. route `get  whoami`

---
+++ WhoAmI/WhoAmI/Controllers/WhoAmIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoAmI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhoAmIController : Controller
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WhoAmIController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string WhoAmI()
+        {
+            var ip = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+                        .Select(p => p.GetIPProperties())
+                        .SelectMany(p => p.UnicastAddresses)
+                        .Where(p => p.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && !System.Net.IPAddress.IsLoopback(p.Address))
+                        .FirstOrDefault()?.Address.ToString();
+
+            _logger.LogInformation($"This is return {ip}");
+            return ip;
+        }
+    }
+}
